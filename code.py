4ce9cfc8f5f43518254c3fbe9bf633a53ddbc0da
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-03-12 11:22:44 -0300

    Update code.py

--- code.py
+++ code.py
@@ -24,4 +24,29 @@ def saque():
 
   elif sacar > 0:
     saldo = saldo - sacar
-    print(f"O saque de R$ {sacar:.2f} foi efe
+    print(f"O saque de R$ {sacar:.2f} foi efetuado com sucesso!")
+
+  else:
+    print("Valor inválido! Por favor, digite um valor positivo!")
+
+def menu_principal():
+  while True:
+    print("\n------ MENU PRINCIPAL ------")
+    print("1 - Ver Saldo")
+    print("2 - Depositar")
+    print("3 - Sacar")
+    print("4 - Sair")
+
+    opcao = input("\nEscolha uma opção: ")
+    if opcao == '1':
+      ver_saldo()
+    elif opcao == '2':
+      depositar()
+    elif opcao == '3':
+      saque()
+    elif opcao == '4':
+      break
+    else:
+      print("Número inválido! Tente novamente.")
+
+menu_principal()
